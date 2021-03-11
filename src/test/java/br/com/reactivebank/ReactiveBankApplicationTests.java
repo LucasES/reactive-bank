c7ac9d78559a3ef1caf53693d7b41d2c7730aa07
Lucas Araújo <lucas.compufc@gmail.com>
2021-03-11 16:39:12 -0300

    Finalizando container

--- src/test/java/br/com/reactivebank/ReactiveBankApplicationTests.java
+++ src/test/java/br/com/reactivebank/ReactiveBankApplicationTests.java
@@ -1,13 +1,5 @@
 package br.com.reactivebank;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ReactiveBankApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
